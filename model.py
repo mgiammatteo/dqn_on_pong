6084c2d317cb65c904830e7a6aea56115ceb248f
Max Giammatteo <mgiammatteo@hotmail.com>
2019-01-19 16:27:50 +0000

    Add files via upload

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, in_channels, action_size, seed, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        
+        # 7 = (W-F)/S + 1; (9-3)/1 +1, where 9 is the output of the second conv layer.
+        # 7 is then the ouput of the third conv layer according to the above formula.
+        self.fc1 = nn.Linear(in_features=7*7*64, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=6)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        
+        # prep for linear layer
+        # flatten the inputs into a vector
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+   
+        return x
